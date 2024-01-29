4d2edbebff533437943e32bd04e27fb8d6951ca5
one1putt <klericksonmn@gmail.com>
2024-01-28 23:07:49 -0600

    Tried to remove eventListeners
    tried to add logic that, when a winner is determined, the eventListeners are removed so the user cannot continue to click the buttons and play.  Tried a bunch of things but could not get it working. Left the code in.

--- javascript.js
+++ javascript.js
@@ -4,18 +4,41 @@ const score = document.querySelector('.score')
 let wins = 0
 let losses = 0
 
-buttons.forEach((button) => button.addEventListener('click', () => {
-    (playRound(button.textContent, getComputerChoice())) ? wins++ : losses++
-    if (wins === 5 || losses === 5) {
-        if (wins === 5) {
-            score.textContent = `You won the game ${wins} to ${losses}.`
-        } else {
-            score.textContent = `Sorry, you lost the game ${wins} to ${losses}.`
-        }
-    } else {
-        score.textContent = `Score: you - ${wins} computer - ${losses}.`
+
+const clickFunction = (button) => {
+    return function () { 
+    game(playRound(button.textContent, getComputerChoice()))
     }
-    }))
+}
+
+buttons.forEach((button) => button.addEventListener('click', clickFunction(button)))
+
+function removeListeners() {
+    buttons.forEach((button) => button.removeEventListener('click', clickFunction(button)))
+}
+
+function game(result) {
+    result ? wins++ : losses++;
+
+    if (wins === 5 || losses === 5) {
+        removeListeners();
+        showGameResult();
+    } else {
+        updateScore();
+    }
+}
+
+function showGameResult() {
+    if (wins === 5) {
+        score.textContent = `You won the game ${wins} to ${losses}.`;
+    } else {
+        score.textContent = `Sorry, you lost the game ${wins} to ${losses}.`;
+    }
+}
+
+function updateScore() {
+    score.textContent = `Score: you - ${wins} computer - ${losses}.`;
+}
 
 function getComputerChoice () {
     let choices = ['rock', 'paper', 'scissors']
@@ -47,42 +70,42 @@ function playRound (player, computer) {
     return result
 }
 
-function game () {
-    let playerSelection
-    let computerSelection
-    // intro to game
-    // console.log("let's play Rock, Paper, Scissors...best of 5")
-    // game to run 5 times (best of 5)
+// function game () {
+//     let playerSelection
+//     let computerSelection
+//     // intro to game
+//     // console.log("let's play Rock, Paper, Scissors...best of 5")
+//     // game to run 5 times (best of 5)
 
-    // prompt for user input
-    // playerSelection = prompt('Enter your move:')
-    // get computerSelection
-    // computerSelection = getComputerChoice()
-    // playRound and determine winner
-    let result = playRound(playerSelection, getComputerChoice())
-    result ? wins++ : losses++
+//     // prompt for user input
+//     // playerSelection = prompt('Enter your move:')
+//     // get computerSelection
+//     // computerSelection = getComputerChoice()
+//     // playRound and determine winner
+//     let result = playRound(playerSelection, getComputerChoice())
+//     result ? wins++ : losses++
     
-    // if tie have computer rechoose
-    // playRound again
-    // track winner
+//     // if tie have computer rechoose
+//     // playRound again
+//     // track winner
 
-    // if played three times or more check for winner
-    // if winner end game
-}
-
-// report winner
-// game()
-
-// interesting: I have not been using ;'s' but without it after the game call above, I get a TypeError that game is not a function. the error comes after game() executes and the lines below try to get executed. if i add the semi it works fine. If I do a formal if/else (see below) then it works without the semi; if i eliminate the () around wins === 3 then it works without the semi. I read that you have to be careful of lines that start with a ( since it thinks this is part of the previous line.
-
-wins === 3 ? console.log(`Congrats!! You won ${wins} to ${losses}.`) : console.log(`You lost ${wins} to ${losses}.`)
-
-// if (wins === 3) {
-//     console.log(`Congrats!! You won ${wins} to ${losses}.`)
+//     // if played three times or more check for winner
+//     // if winner end game
 // }
-// else {
-//     console.log(`You lost ${wins} to ${losses}.`)
-// }
+
+// // report winner
+// // game()
+
+// // interesting: I have not been using ;'s' but without it after the game call above, I get a TypeError that game is not a function. the error comes after game() executes and the lines below try to get executed. if i add the semi it works fine. If I do a formal if/else (see below) then it works without the semi; if i eliminate the () around wins === 3 then it works without the semi. I read that you have to be careful of lines that start with a ( since it thinks this is part of the previous line.
+
+// wins === 3 ? console.log(`Congrats!! You won ${wins} to ${losses}.`) : console.log(`You lost ${wins} to ${losses}.`)
+
+// // if (wins === 3) {
+// //     console.log(`Congrats!! You won ${wins} to ${losses}.`)
+// // }
+// // else {
+// //     console.log(`You lost ${wins} to ${losses}.`)
+// // }
 
 
 
